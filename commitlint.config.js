618c75e84a5cfc92fc5f312cf6e023fc5097a040
ytppp <651003708@qq.com>
2022-10-10 11:26:35 +0800

    feat: 验证commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,8 +2,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "type-enum": [2, "always", ["feat", "fix", "revert"]],
-    "subject-max-length": [1, "always", 30],
-    "subject-empty": [0],
-    "type-empty": [0]
+    "subject-max-length": [1, "always", 30]
   },
 };
